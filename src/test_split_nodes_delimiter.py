43077f32257cf47ad00e37de96953f9b8ec3f3c5
Harold Hernandez <harold.hernandez@visma.com>
2024-11-10 20:31:54 +0100

    added tests for multiple delimiters in a line

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -39,3 +39,50 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             ]
         
         self.assertEqual(expected, new_nodes)
+
+    
+    def test_multiple_delimited_line(self):
+        
+        node = TextNode("*bold* `code` _you_ got there", TextType.NORMAL)
+        bold_node = split_nodes_delimiter([node], "*", TextType.BOLD)
+
+        self.assertEqual([
+            TextNode("bold", TextType.BOLD),
+            TextNode(" `code` _you_ got there", TextType.NORMAL)
+        ], bold_node)
+
+        
+        code_node = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual([
+            TextNode("*bold* ", TextType.NORMAL),
+            TextNode("code", TextType.CODE),
+            TextNode(" _you_ got there", TextType.NORMAL)
+        ], code_node)
+
+        
+        
+        code_node = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual([
+            TextNode("*bold* `code` ", TextType.NORMAL),
+            TextNode("you", TextType.ITALIC),
+            TextNode(" got there", TextType.NORMAL)
+        ], code_node)
+
+        # code_node = split_nodes_delimiter([node], "`", TextType.CODE)
+        # italic_node = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        # normal_node = [TextNode("got there", TextType.NORMAL)]
+
+        # result = []
+        # result.extend(bold_node)
+        # result.extend(code_node)
+        # result.extend(italic_node)
+        # result.extend(normal_node)
+
+        # expected = [
+        #     TextNode("bold ", TextType.BOLD),
+        #     TextNode("code ", TextType.CODE),
+        #     TextNode("you ", TextType.ITALIC),
+        #     TextNode("got there", TextType.NORMAL),
+        #     ]
+        
+        # self.assertEqual(expected, result)
