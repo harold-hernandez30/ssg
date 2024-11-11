c54b167189e4c16a71e24067157c40d9acbcee42
Harold Hernandez <harold.hernandez@visma.com>
2024-11-11 20:50:02 +0100

    improve readability

--- src/extract_markdown_images.py
+++ src/extract_markdown_images.py
@@ -1,6 +1,10 @@
 import re
 
 def extract_markdown_images(text):
-    alt_text_list = re.findall(r"\[(.*?)\]", text)
+    regex_image_alt_text = r"\[(.*?)\]"
+    alt_text_list = re.findall(regex_image_alt_text, text)
 
-    return list(map(lambda alt_text: (alt_text, re.findall(rf"\[{alt_text}\]\((.*?)\)", text)[0]), alt_text_list))
+    def extract_image_url(alt_text, line):
+        return re.findall(rf"\[{alt_text}\]\((.*?)\)", line)
+    
+    return list(map(lambda alt_text: (alt_text, extract_image_url(alt_text, text)[0]), alt_text_list))
